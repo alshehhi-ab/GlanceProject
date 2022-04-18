621bb12d2bc10a23a2cdbc4276f8cf1c3f8a625c
alshehhi-ab <alshehhi.ab@northeastern.edu>
2022-04-18 12:08:22 -0400

    Initial commit for main.py

---
+++ GlanceProject/main.py
@@ -0,0 +1,19 @@
+from task import Task
+from event import Event
+from youm import Youm
+
+debug = True
+
+
+
+
+
+
+# Test output for Task
+
+if debug:
+    task_a = Task()
+    task_a.set_name("Test Task")
+    task_a.set_description("The quick brown fox jumped over the lazy dog.")
+    task_a.set_priority("Medium")
+    print(a)
